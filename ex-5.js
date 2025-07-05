90b1c5849e9506381dbeb82f94df9f263e0bf4a3
fn <j.k.13@hotmail.com>
2025-07-06 03:46:10 +0700

    feat: assignment primitive values

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let discountRate = productPrice * 0.25;
+let totalDiscountPrice = productPrice - discountRate;
+console.log(totalDiscountPrice); // totalDiscountPrice = 20250
